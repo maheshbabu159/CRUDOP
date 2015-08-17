68df4b16f3e911ced616e38580d013775db22460
maheshbabu159 <maheshbabu.somineni@gmail.com>
2015-08-17 18:39:24 +0530

    g

--- header.php
+++ header.php
@@ -9,33 +9,46 @@
     </head>
 
     <body>
-
         <div class="navbar navbar-default navbar-static-top" role="navigation">
+            
             <div class="container">
+                <!-- Brand and toggle get grouped for better mobile display -->
                 <div class="navbar-header">
-                     <ul class="nav navbar-nav">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="http://localhost:8080/CRUDOP/index.php#"><img src="http://placehold.it/140x34/000000/ffffff/&amp;text=LOGO" alt=""></a>
+                </div>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <form class="navbar-form navbar-right" role="search">
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Search">
+                        </div>
+                        <button type="submit" class="btn btn-default">Submit</button>
+                    </form>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#">Link</a></li>
                         <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Iasmani Pinazo <span class="glyphicon glyphicon-user pull-right"></span></a>
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
                             <ul class="dropdown-menu">
-                                <li><a href="#">Account Settings <span class="glyphicon glyphicon-cog pull-right"></span></a></li>
+                                <li><a href="#">Action</a></li>
+                                <li><a href="#">Another action</a></li>
+                                <li><a href="#">Something else here</a></li>
                                 <li class="divider"></li>
-                                <li><a href="#">User stats <span class="glyphicon glyphicon-stats pull-right"></span></a></li>
-                                <li class="divider"></li>
-                                <li><a href="#">Messages <span class="badge pull-right"> 42 </span></a></li>
-                                <li class="divider"></li>
-                                <li><a href="#">Favourites Snippets <span class="glyphicon glyphicon-heart pull-right"></span></a></li>
-                                <li class="divider"></li>
-                                <li><a href="#">Sign Out <span class="glyphicon glyphicon-log-out pull-right"></span></a></li>
+                                <li><a href="#">Separated link</a></li>
                             </ul>
                         </li>
                     </ul>
-                    
-                </div>
-         
+                </div><!-- /.navbar-collapse -->
+                  
             </div>
         </div>
         <div class="container">
-
+           
             <ul class="nav nav-tabs">
                 <li class="active"><a data-toggle="tab" href="#home">Home</a></li>
                 <li><a data-toggle="tab" href="#menu1">Menu 1</a></li>
